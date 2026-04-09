031b9b9c46ee67d387031f318069c0a764d046c1
Diego Pinheiro <diego.olliveirap@gmail.com>
2026-04-09 20:57:16 -0300

    feat: add user column on database and add migration

---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+from app.ext.database import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, nullable=False, default=db.func.now(), onupdate=db.func.now()
+    )
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
